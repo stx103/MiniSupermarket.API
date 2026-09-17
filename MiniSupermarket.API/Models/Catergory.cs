8b78dcdd36bd01d382ed557b88c77a8a6afb0566
Admin <Admin@D20828>
2026-09-17 19:17:25 +0700

    Add project files.

---
+++ MiniSupermarket.API/Models/Catergory.cs
@@ -0,0 +1,14 @@
+﻿namespace MiniSupermarket.API.Models
+{
+    public class Category
+    {
+        // Mã định danh nhóm hàng (Khóa chính)
+        public int CategoryId { get; set; }
+
+        // Tên nhóm hàng (Bắt buộc, không được để trống)
+        public string CategoryName { get; set; } = string.Empty;
+
+        // Mô tả chi tiết về nhóm hàng (Có thể để trống)
+        public string? Description { get; set; }
+    }
+}
---
+++ MiniSupermarket.WinForms/FormCategoryManagement.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MiniSupermarket.WinForms
+{
+    partial class FormCategoryManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvCategories = new DataGridView();
+            txtId = new TextBox();
+            txtCategoryName = new TextBox();
+            txtDescription = new TextBox();
+            txtKeyword = new TextBox();
+            btnLoad = new Button();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvCategories
+            // 
+            dgvCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCategories.Location = new Point(12, 95);
+            dgvCategories.Name = "dgvCategories";
+            dgvCategories.Size = new Size(446, 343);
+            dgvCategories.TabIndex = 0;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(601, 86);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(132, 23);
+            txtId.TabIndex = 1;
+            // 
+            // txtCategoryName
+            // 
+            txtCategoryName.Location = new Point(601, 115);
+            txtCategoryName.Name = "txtCategoryName";
+            txtCategoryName.Size = new Size(132, 23);
+            txtCategoryName.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(601, 147);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(132, 23);
+            txtDescription.TabIndex = 3;
+            // 
+            // txtKeyword
+            // 
+            txtKeyword.Location = new Point(12, 50);
+            txtKeyword.Name = "txtKeyword";
+            txtKeyword.Size = new Size(293, 23);
+            txtKeyword.TabIndex = 4;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(383, 50);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 5;
+            btnLoad.Text = "tai lai";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(529, 215);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 6;
+            btnAdd.Text = "themmoi";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(621, 215);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 7;
+            btnUpdate.Text = "update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(713, 215);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 8;
+            btnDelete.Text = "xoa";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(302, 50);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 23);
+            btnSearch.TabIndex = 9;
+            btnSearch.Text = "tim kiem";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // FormCategoryManagement
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSearch);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(btnLoad);
+            Controls.Add(txtKeyword);
+            Controls.Add(txtDescription);
+            Controls.Add(txtCategoryName);
+            Controls.Add(txtId);
+            Controls.Add(dgvCategories);
+            Name = "FormCategoryManagement";
+            Text = "FormCategoryManagement";
+            Load += FormCategoryManagement_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvCategories;
+        private TextBox txtId;
+        private TextBox txtCategoryName;
+        private TextBox txtDescription;
+        private TextBox txtKeyword;
+        private Button btnLoad;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnSearch;
+    }
+}
